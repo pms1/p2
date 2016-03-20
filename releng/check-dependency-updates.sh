1366514eac6ff144e25e4f16725a0d6b56fbecb3
Mirko Streckenbach <pms1@users.noreply.github.com>
2016-03-20 07:41:05 +0100

    restrict to p2.parent subdirectory, this is enough at the moment

--- releng/check-dependency-updates.sh
+++ releng/check-dependency-updates.sh
@@ -1,5 +1,5 @@
-mvn versions:display-dependency-updates -Dtycho.mode=maven
+cd p2.parent && mvn versions:display-dependency-updates -Dtycho.mode=maven
 
 # scans a project's dependencies and
 # produces a report of those dependencies which have newer versions available.
-# versions:display-plugin-updates 
+# mvn versions:display-plugin-updates  -Dtycho.mode=maven
